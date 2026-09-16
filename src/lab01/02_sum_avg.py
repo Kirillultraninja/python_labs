da7f626734a8ef336d2e43930f49745481f8f161
Kirillultraninja <kirillepetrin@yandex.ru>
2026-09-16 21:56:21 +0300

    zad02

--- src/lab01/02_sum_avg.py
+++ src/lab01/02_sum_avg.py
@@ -1,7 +1,15 @@
-a= input("a: ")
-b= input("b: ")
-a1= float(a[0]+"."+a[2:])
-b1= float(b[0]+"."+b[2:])
-sum=a1+b1
+a=input("a: ")
+b=input("b: ")
+if "," in a:
+    a1=list(map(str, a.split(',')))
+    a2= float(str(a1[0])+"."+str(a1[1]))
+else:
+    a2=float(a)
+if "," in b:    
+    b1=list(map(str, b.split(',')))
+    b2= float(str(b1[0])+"."+str(b1[1]))
+else:
+    b2=float(b)
+sum=a2+b2
 avg= round(sum/2,2)
 print(f"sum={sum};",f"avg={avg}")
