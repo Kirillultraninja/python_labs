d3c6558698d673c1e6c001735502ec1aa5fdc71c
Kirillultraninja <kirillepetrin@yandex.ru>
2026-09-18 15:00:13 +0300

    zad04

---
+++ src/lab01/05_initials_and_len.py
@@ -0,0 +1,7 @@
+fio= list(map(str,input("ФИО: ").split(" ")))
+b=[]
+for i in fio:
+    if i!= "":
+        b.append(i)
+print("Инициалы:",b[0][0]+b[1][0]+b[2][0])
+print("Длина (символов):", len(b[0])+len(b[1])+len(b[2]))
