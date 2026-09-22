c00ca6ef7c3fab9ad6eb8cbbcabb577753322033
Kirillultraninja <kirillepetrin@yandex.ru>
2026-09-22 15:03:07 +0300

    fix 7

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -4,4 +4,4 @@ for i in fio:
     if i!= "":
         b.append(i)
 print("Инициалы:",b[0][0]+b[1][0]+b[2][0])
-print("Длина (символов):", len(b[0])+len(b[1])+len(b[2]))
+print("Длина (символов):", len(b[0])+len(b[1])+len(b[2])+2)
